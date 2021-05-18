4c646c1538c3b78dcac4641f68fd5b4266e0b719
domeki <domeki151190@gmail.com>
2021-05-18 19:36:02 -0400

    x

--- tareaSilla/tareaSilla/Program.cs
+++ tareaSilla/tareaSilla/Program.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using OpenTK;
 namespace tareaSilla
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola mundo");
-            Console.ReadKey();
+            GameWindow window = new GameWindow(500, 500);
+            Silla chair = new Silla(window);
         }
     }
 }
